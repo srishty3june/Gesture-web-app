07641f70040684111e317e412d3257d40c37bcbc
srishty3june <93928989+srishty3june@users.noreply.github.com>
2022-10-31 17:01:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -34,3 +34,46 @@ function modelLoaded() {
 
     console.log('Model Loaded!');
 }
+
+function check() {
+
+  img = document.getElementById("captured_image");
+  classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+        predcition_1 = results[0].label;
+        predcition_2 = results[1].label;
+        speak();
+        if (results[0].label == "best") {
+            document.getElementById("update_gesture").innerHTML = "&#128077;";
+        }
+
+        if (results[0].label == "amazing") {
+            document.getElementById("update_gesture").innerHTML = "&#128076;";
+        }
+
+        if (results[0].label == "victory") {
+            document.getElementById("update_gesture").innerHTML = "&#9996;";
+        }
+
+        if (results[1].label == "best") {
+            document.getElementById("update_gesture2").innerHTML = "&#128077;";
+        }
+
+        if (results[1].label == "amazing") {
+            document.getElementById("update_gesture2").innerHTML = "&#128076;";
+        }
+
+        if (results[1].label == "victory") {
+            document.getElementById("update_gesture2").innerHTML = "&#9996;";
+        }
+    }
+}
